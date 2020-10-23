c8ca9914b1ceeafa3ac99b9f4e5dd86572daa32f
jundymek <jundymek@gmail.com>
2020-10-23 15:00:58 +0200

    feat: ChatRoom displays messages

--- components/chatRoom/ChatRoom.tsx
+++ components/chatRoom/ChatRoom.tsx
@@ -1,14 +1,74 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { RouteProp } from "@react-navigation/native";
+import { gql, useQuery } from "@apollo/client";
 
 interface Props {
   route: RouteProp<{ params: { chatId: string } }, "params">;
 }
 
+export const GET_ROOM = gql`
+  query Room($chatId: String!) {
+    room(id: $chatId) {
+      id
+      name
+      messages {
+        body
+        id
+        insertedAt
+        user {
+          email
+          firstName
+          id
+          lastName
+          role
+        }
+      }
+      user {
+        email
+        firstName
+        id
+        lastName
+        role
+      }
+    }
+  }
+`;
+interface User {
+  __typename: string;
+  email: string;
+  firstName: string;
+  id: string;
+  lastName: string;
+  role: string;
+}
+interface Message {
+  body: string;
+  id: string;
+  insertedAt: string;
+  user: User;
+}
+
 export const ChatRoom = React.memo<Props>(({ route }) => {
-  console.log(route.params);
-  return <Text>Chat room {route.params.chatId}</Text>;
+  const { chatId } = route.params;
+  console.log(chatId);
+  const { data, loading } = useQuery(GET_ROOM, { variables: { chatId } });
+
+  console.log(data && data.room.messages);
+
+  if (loading) return <Text>Loading...</Text>;
+  return (
+    <View>
+      {data &&
+        data.room.messages.map((message: Message) => {
+          return (
+            <Text>
+              {message.body} - {message.user.firstName} {message.user.lastName}{" "}
+            </Text>
+          );
+        })}
+    </View>
+  );
 });
 
 ChatRoom.displayName = "ChatRoom";
